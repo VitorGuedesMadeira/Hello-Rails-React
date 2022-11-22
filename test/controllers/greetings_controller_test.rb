3491372de02ca389e9038c4ee4b789853b6093ed
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-22 17:29:19 -0300

    Fix rubocop

--- test/controllers/greetings_controller_test.rb
+++ test/controllers/greetings_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class GreetingsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
--- test/controllers/root_controller_test.rb
+++ test/controllers/root_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class RootControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get root_index_url
     assert_response :success
   end
